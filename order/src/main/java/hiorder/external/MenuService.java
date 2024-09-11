e7ec995a93a145dd77b6a2da0ceef1c07ea0600b
jaeung lee <jaeunglee23@gmail.com>
2024-09-11 07:05:14 +0000

    Refactor

--- order/src/main/java/hiorder/external/MenuService.java
+++ order/src/main/java/hiorder/external/MenuService.java
@@ -1,21 +1,18 @@
-
 package hiorder.external;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.Date;
-import java.util.List;
-
-
 @FeignClient(name = "menu", url = "${api.url.menu}")
- 
 public interface MenuService {
+    
+    // URL 경로에서 id를 추출해 Long 타입으로 전달
     @GetMapping(path="/menus/search/findByGetMenu/{id}")
-    public Menu getMenu(@PathVariableGetMenuQuery getMenuQuery);
+    public Menu getMenu(@PathVariable("id") Long id);
 
 }
+
 
 
 
